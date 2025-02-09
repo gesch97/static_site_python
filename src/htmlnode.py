34a5ebecc0a3de311f9e47af73d0afe1dfd38232
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-02-09 22:55:30 +0100

    add text to HTML conversion + tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,6 @@
+from textnode import TextNode, TextType
+
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -19,3 +22,31 @@ class HTMLNode:
 
     def __repr__(self):
         text = f"""HTMLNode({self.tag} {self.value} {self.children} {self.props}"""
+
+    @classmethod
+    def from_text_node(cls, text_node: TextNode):
+        if not isinstance(text_node, TextNode):
+            raise ValueError(f"text_node should be TextNode, not: {type(text_node)}")
+
+        match text_node.text_type:
+            case TextType.NORMAL:
+                return cls(value=text_node.text)
+            case TextType.BOLD:
+                return cls(tag="b", value=text_node.text)
+            case TextType.ITALIC:
+                return cls(tag="i", value=text_node.text)
+            case TextType.CODE:
+                return cls(tag="code", value=text_node.text)
+            case TextType.LINK:
+                return cls(tag="a", value=text_node.text, props={"href": text_node.url})
+            case TextType.IMAGE:
+                return cls(
+                    tag="img",
+                    value="",
+                    props={
+                        "src": text_node.url,
+                        "alt": text_node.text,
+                    },
+                )
+            case _:
+                raise ValueError("Unknow TextType")
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from textnode import TextNode, TextType
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -23,3 +24,52 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         text = node.props_to_html()
         self.assertEqual(text, "")
+
+    def test_from_text_node(self):
+        text_node_normal = TextNode("asd", TextType.NORMAL)
+        text_node_bold = TextNode("asd", TextType.BOLD)
+        text_node_italic = TextNode("asd", TextType.ITALIC)
+        text_node_code = TextNode("asd", TextType.CODE)
+        text_node_link = TextNode("asd", TextType.LINK, "google.com")
+        text_node_image = TextNode("A cat", TextType.IMAGE, url="cat.png")
+
+        html_node_image = HTMLNode.from_text_node(text_node_image)
+        html_node_link = HTMLNode.from_text_node(text_node_link)
+        html_node_code = HTMLNode.from_text_node(text_node_code)
+        html_node_italic = HTMLNode.from_text_node(text_node_italic)
+        html_node_bold = HTMLNode.from_text_node(text_node_bold)
+        html_node_normal = HTMLNode.from_text_node(text_node_normal)
+
+        self.assertIsNone(html_node_normal.tag)
+        self.assertEqual(html_node_normal.value, "asd")
+
+        self.assertEqual(html_node_bold.tag, "b")
+        self.assertEqual(html_node_bold.value, "asd")
+
+        self.assertEqual(html_node_italic.tag, "i")
+        self.assertEqual(html_node_italic.value, "asd")
+
+        self.assertEqual(html_node_code.tag, "code")
+        self.assertEqual(html_node_code.value, "asd")
+
+        self.assertEqual(html_node_link.tag, "a")
+        self.assertEqual(html_node_link.value, "asd")
+        self.assertEqual(
+            html_node_link.props,
+            {
+                "href": "google.com",
+            },
+        )
+
+        self.assertEqual(html_node_image.tag, "img")
+        self.assertEqual(html_node_image.value, "")
+        self.assertEqual(
+            html_node_image.props,
+            {
+                "src": "cat.png",
+                "alt": "A cat",
+            },
+        )
+
+        with self.assertRaises(ValueError):
+            HTMLNode.from_text_node("not a text_node")
