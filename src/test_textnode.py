b5e2b67a0b6841620bce66680c4f309a5102a669
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-02-10 11:27:06 +0100

    add TextNode.split_nodes_delimeter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -23,6 +23,60 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("asd", TextType.BOLD)
         self.assertEqual(node.url, None)
 
+    def test_split_nodes_delimeter(self):
+        with self.assertRaises(TypeError):
+            TextNode.split_nodes_delimeter("not a list", "a", TextType.NORMAL)
+
+        with self.assertRaises(ValueError):
+            TextNode.split_nodes_delimeter(
+                [
+                    TextNode("a", TextType.NORMAL),
+                ],
+                "",
+                TextType.NORMAL,
+            )
+
+        with self.assertRaises(ValueError):
+            TextNode.split_nodes_delimeter(
+                [
+                    TextNode("a", TextType.NORMAL),
+                ],
+                "*",
+                "not a texttype",
+            )
+
+        node = TextNode("This is text with a `code block` word", TextType.NORMAL)
+        node_split = TextNode.split_nodes_delimeter(
+            [
+                node,
+            ],
+            "`",
+            TextType.CODE,
+        )
+        should_return = [
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.NORMAL),
+        ]
+
+        self.assertEqual(node_split, should_return)
+
+        node2 = TextNode("aa 'bb' cc' dd", TextType.NORMAL)
+        node_split2 = TextNode.split_nodes_delimeter(
+            [
+                node2,
+            ],
+            "'",
+            TextType.CODE,
+        )
+        should_return2 = [
+            TextNode("aa ", TextType.NORMAL),
+            TextNode("bb", TextType.CODE),
+            TextNode(" cc", TextType.NORMAL),
+            TextNode(" dd", TextType.CODE),
+        ]
+        self.assertEqual(node_split2, should_return2)
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from typing import Self
+import re
 
 
 class TextType(Enum):
@@ -25,3 +27,29 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text} {self.text_type} {self.url})"
+
+    @staticmethod
+    def split_nodes_delimeter(
+        old_nodes: list[Self], delimeter: str, text_type: TextType
+    ) -> list[Self]:
+        if not isinstance(old_nodes, list):
+            raise TypeError(
+                f"input 'old_nodes' should be a list, not {type(old_nodes)}"
+            )
+        if len(delimeter) <= 0:
+            raise ValueError("Provided delimeter was an empty string.")
+
+        if not isinstance(text_type, TextType):
+            raise ValueError("Incorrect text_type")
+
+        delimeter_escaped = re.escape(delimeter)
+        output = list()
+        for node in old_nodes:
+            node_split_at_delimeters = re.split(delimeter_escaped, node.text)
+            for i, piece in enumerate(node_split_at_delimeters):
+                if i % 2 == 0:
+                    new_textnode = TextNode(piece, node.text_type)
+                else:
+                    new_textnode = TextNode(piece, text_type)
+                output.append(new_textnode)
+        return output
