efc6365f847770bf41061a38697d6465876897f3
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-02-15 16:44:13 +0100

    add function to find out the type of markdown block

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,17 @@
+import enum
+import re
+
+
+class BlockType(enum.Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+    NORMAL = "normal"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     if not isinstance(markdown, str):
         raise ValueError("Input should be string")
@@ -10,3 +24,42 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         output.append(without_trailing_or_leading_white_space)
 
     return output
+
+
+def block_to_block_type(block: str):
+    if not isinstance(block, str):
+        raise TypeError("input should be string")
+
+    re_pat_heading = r"#{1,6}[ ].*"
+    if re.fullmatch(re_pat_heading, block) is not None:
+        return BlockType.HEADING
+
+    re_pat_code = r"^`{3}.*`{3}"
+    if re.fullmatch(re_pat_code, block) is not None:
+        return BlockType.CODE
+
+    re_pat_quote = r"^>.*"
+    if re.match(re_pat_quote, block, flags=re.MULTILINE) is not None:
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.NORMAL
+        return BlockType.QUOTE
+
+    re_pat_u_ordered_list = r"^\*.*"
+    if re.match(re_pat_u_ordered_list, block, flags=re.MULTILINE) is not None:
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith("* "):
+                return BlockType.NORMAL
+        return BlockType.UNORDERED_LIST
+
+    re_pat_ordered_list = r"^\d\.\s.*\n"
+    if re.match(re_pat_ordered_list, block, flags=re.MULTILINE) is not None:
+        lines = block.split("\n")
+        for i, line in enumerate(lines):
+            if not line.startswith(str(i + 1)):
+                return BlockType.NORMAL
+        return BlockType.ORDERED_LIST
+
+    return BlockType.NORMAL
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown import markdown_to_blocks
+from markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class test_markdown(unittest.TestCase):
@@ -30,3 +30,29 @@ class test_markdown(unittest.TestCase):
 
         self.assertEqual(result, expected_result)
         self.assertEqual(result2, expected_result2)
+
+    def test_markdown_to_blocks(self):
+        test_cases = [
+            ("#This not a heading", BlockType.NORMAL),
+            ("# This a heading", BlockType.HEADING),
+            ("## This a heading", BlockType.HEADING),
+            ("#### This a heading", BlockType.HEADING),
+            ("##### This a heading", BlockType.HEADING),
+            ("###### This a heading", BlockType.HEADING),
+            ("``` This a code block```", BlockType.CODE),
+            ("`` This a code block```", BlockType.NORMAL),
+            ("``` This a code block``", BlockType.NORMAL),
+            ("> This a single line of quote", BlockType.QUOTE),
+            ("> This a single line of quote\n>This a second one", BlockType.QUOTE),
+            ("> This a single line of quote\nThis a second one", BlockType.NORMAL),
+            ("* This an unordered list element", BlockType.UNORDERED_LIST),
+            ("* This an unordered list element\n* asd", BlockType.UNORDERED_LIST),
+            ("* This an unordered list element\n asd", BlockType.NORMAL),
+            ("1. first\n2. second\n3. third", BlockType.ORDERED_LIST),
+            ("1. first\n4. second\n3. third", BlockType.NORMAL),
+            ("This\n is a normal\n block", BlockType.NORMAL),
+        ]
+
+        for test_case in test_cases:
+            res = block_to_block_type(test_case[0])
+            self.assertEqual(res, test_case[1])
